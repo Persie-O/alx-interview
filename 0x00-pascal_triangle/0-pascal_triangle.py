0a24ec9426edaaf36116f283a7a3454a06487058
Pascal <persiewailer697@gmail.com>
2023-09-29 05:12:41 +0300

    Pascal's traingle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python3
-"""Writing a function for Pascal's Triangle"""
+"""
+function def pascal_triangle(n): that returns a list of lists of integers
+representing the Pascal's triangle of n
+"""
 
 
 def pascal_triangle(n):
-    # Edge case: Return an empty list for n <= 0
+    """returns a list of lists of integers representing the Pascal's
+    triangle of n
+
+    Args:
+        n (integer): number of rows
+
+    Returns:
+        list: integers representing Pascal's triangle.
+    """
+    # if n is less or 0 return an empty list
     if n <= 0:
         return []
 
-    # Initialize the Pascal's triangle with the first row
-    triangle = [[1]]
+    else:
+        # Initialize a variable called triangle with a list containing a
+        # single element, [1], which represents the first row of the triangle.
+        triangle = [[1]]
 
-    # Build Pascal's triangle row by row
-    for i in range(1, n):
-        # Calculate the current row based on the previous row
-        prev_row = triangle[-1]  # Get the previous row
-        current_row = [1]  # Initialize the current row with the leftmost 1
-        for j in range(1, i):
-            # Calculate the elements between the leftmost and rightmost 1's
-            current_row.append(prev_row[j - 1] + prev_row[j])
-        current_row.append(1)  # Add the rightmost 1
-        triangle.append(current_row)  # Add the current row to the triangle
+        for i in range(1, n):
+            # On each iteration of the loop, a new list is created and
+            # appended to the triangle variable. This list starts with a
+            # single element, 1, which represents the first element of the
+            # new row.
+            triangle.append([1])
 
-    return triangle
+            # The function then enters a nested loop that iterates over the
+            # elements in the previous row (the row with an index of i-1) and
+            # calculates the value of each element in the new row (the row
+            # with an index of i) based on the sum of the elements in the
+            # previous row that are directly above and to the left and right
+            # of the current element.
+            for j in range(1, i):
+                triangle[i].append(triangle[i-1][j-1] + triangle[i-1][j])
+
+            # This process continues until all of the elements in the new row
+            # have been calculated, at which point a final 1 is appended to
+            # the end of the row to complete it.
+            triangle[i].append(1)
+        return triangle
